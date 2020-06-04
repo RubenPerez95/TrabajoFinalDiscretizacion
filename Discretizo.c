7bdbb23d942942c7eff5b483f715ef6e81df8550
Ruben Perez Rubio <ruben.perez7@alu.uclm.es>
2020-06-04 20:29:44 +0200

    Modificaciones formato salida

--- Discretizo.c
+++ Discretizo.c
@@ -2,20 +2,18 @@
 #include <time.h>
 
 
-void insertarEdad(int valor, int vector[]);
 void imprimirVectorDiscretizado(int vector[], char tipoEdad);
 
 int main() {
+    
+    int vectorEdades[30]; //Se inicializa el vector de edades con un tamaño de 30
 
-    //Se inicializa el vector de edades con un tamaño de 30
-    int vectorEdades[30];
+    char *vectorSalidaIntervalo[30]; //vector destinado a la disposición de los intervalos a los que pertenece cada edad
+    int vectorSalidaCuentaEdades[4]; //vector destinado a la cuenta de edades en un mismo intervalo
+    int vectorSalidaPosicion[4]; 
+
+
     double tiempoInicio, tiempoFin;
-
-    int vectorInfantil[30];
-    int vectorJuvenil[30];
-    int vectorAdulto[30];
-    int vectorAnciano[30];
-
 
     for (int i=0; i<sizeof(vectorEdades); i++) {
         vectorEdades[i] = rand()%95;
@@ -31,99 +29,63 @@ int main() {
     //Por defecto debería coger 4 siempre. Por si acaso.
     omp_set_num_threads(4);
 
-    int contador1 = 0;
-    int contador2 = 0;
-    int contador3 = 0;
-    int contador4 = 0;
+    int inf = "Infantil";
+    int jov = "Joven";
+    int adu = "Adulto";
+    int may = "Mayor";
 
     tiempoInicio = omp_get_wtime;
-    printf("Tiempo antes del bucle de discretización: %d", tiempoInicio);
     //Bucle serial
     for (int i=0; i<sizeof(vectorEdades); i++) {
             if(vectorEdades[i] >= 0 && vectorEdades[i] <= 14) {
-                insertarEdad(vectorEdades[i], vectorInfantil, contador1)
-                /**vectorInfantil[contador1] = vectorEdades[i];
-                contador1++;**/
+                vectorSalidaIntervalo[i] = inf;
             }
             else if(vectorEdades[i] >= 15 && vectorEdades[i] <= 24) {
-                insertarEdad(vectorEdades[i], vectorJuvenil, contador2)
-                /**vectorJuvenil[contador2] = vectorEdades[i];
-                contador2++;**/
+                vectorSalidaIntervalo[i] = jov;
             }
             else if(vectorEdades[i] >= 25 && vectorEdades[i] <= 64) {
-                insertarEdad(vectorEdades[i], vectorAdulto, contador3)
-                /**vectorAdulto[contador3] = vectorEdades[i];
-                contador3++;**/
+                vectorSalidaIntervalo[i] = adu;
             }
             else {
-                insertarEdad(vectorEdades[i], vectorAnciano, contador4)
-                /**vectorAnciano[contador4] = vectorEdades[i];
-                contador4++;**/
+                vectorSalidaIntervalo[i] = may;
             }
     }
     tiempoFin = omp_get_wtime;
-    printf("Tiempo después del bucle de discretización %d", tiempoInicio);
+    printf("Tiempo ejecución serial:  %d", tiempoFin - tiempoInicio);
 
-
-    int vectorInfantil[30];
-    int vectorJuvenil[30];
-    int vectorAdulto[30];
-    int vectorAnciano[30];
 
     tiempoInicio = omp_get_wtime;
-    printf("Tiempo antes del bucle de discretización: %d", tiempoInicio);
     //Bucle paralelizado
-    int contador1 = 0;
-    int contador2 = 0;
-    int contador3 = 0;
-    int contador4 = 0;
     #pragma omp paralel for shared(vectorEdades, i)
     for (int i=0; i<sizeof(vectorEdades); i++) {
         #pragma omp sections
             #pragma omp section
             if(vectorEdades[i] >= 0 && vectorEdades[i] <= 14) {
-                insertarEdad(vectorEdades[i], vectorInfantil, contador1)
-                /**vectorInfantil[contador1] = vectorEdades[i];
-                contador1++;**/
+                vectorSalidaIntervalo[i] = inf;
             }
             #pragma omp section
             else if(vectorEdades[i] >= 15 && vectorEdades[i] <= 24) {
-                insertarEdad(vectorEdades[i], vectorJuvenil, contador2)
-                /**vectorJuvenil[contador2] = vectorEdades[i];
-                contador2++;**/
+                vectorSalidaIntervalo[i] = jov;
             }
-
             #pragma omp section
             else if(vectorEdades[i] >= 25 && vectorEdades[i] <= 64) {
-                insertarEdad(vectorEdades[i], vectorAdulto, contador3)
-                /**vectorAdulto[contador3] = vectorEdades[i];
-                contador3++;**/
+                vectorSalidaIntervalo[i] = adu;
             }
-
             #pragma omp section
             else {
-                insertarEdad(vectorEdades[i], vectorAnciano, contador4)
-                /**vectorAnciano[contador4] = vectorEdades[i];
-                contador4++;**/
+                vectorSalidaIntervalo[i] = may;
             }
     }
     tiempoFin = omp_get_wtime;
-    printf("Tiempo después del bucle de discretización %d", tiempoInicio);
+    printf("Tiempo ejecución paralelizado:  %d", tiempoFin - tiempoInicio);
 
-    imprimirVectorDiscretizado(vectorInfantil, "Infantes");
-    imprimirVectorDiscretizado(vectorJuvenil, "Jóvenes");
-    imprimirVectorDiscretizado(vectorAdulto, "Adultos");
-    imprimirVectorDiscretizado(vectorAnciano, "Ancianos")
+    imprimirVectorDiscretizado(vectorSalidaIntervalo);
+    /*imprimirVectorDiscretizado(vectorSalidaCuentaEdades);*/
 }
 
-void insertarEdad(int valor, int vector[], int contador) {
-    vector[contador] = valor;
-    contador++;
-}
-
-void imprimirVectorDiscretizado(int vector[], char tipoEdad) {
-    printf("Vector de %c:", tipoEdad);
+void imprimirVectorDiscretizado(int vector[]) {
+    printf("Vector: \n");
     for (int i=0; i<sizeof(vector); i++) {
-        printf("%d", vector[i]);
+        printf("%c", vector[i]);
     }
 }
